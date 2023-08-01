e3ebd63996b70492b69fad93bacded2f57fa491c
Mike Zornek <zorn@zornlabs.com>
2023-08-01 15:33:06 -0400

    Finish the first section of the Ash getting started tutorial.

--- config/config.exs
+++ config/config.exs
@@ -2,3 +2,5 @@ import Config
 
 # Temp Ash config that the docs say will be able to be removed in the future.
 config :ash, :use_all_identities_in_manage_relationship?, false
+
+config :helpdesk, :ash_apis, [Helpdesk.Support]
